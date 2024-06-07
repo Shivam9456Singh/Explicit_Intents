fae11845e5d220ab02d78da1d6f8e9ae4fe06164
Shivam Kumar Singh <113454708+Shivam9456Singh@users.noreply.github.com>
2024-06-07 14:34:32 +0530

    Initial commit

---
+++ app/src/main/java/com/martinestudio/textandscrollview/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.martinestudio.textandscrollview
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+         val buttonNew = findViewById<Button>(R.id.btnNew)
+
+        buttonNew.setOnClickListener{
+            val intent = Intent(applicationContext, new_intent::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/martinestudio/textandscrollview/new_intent.kt
@@ -0,0 +1,29 @@
+package com.martinestudio.textandscrollview
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class new_intent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_new_intent)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val buttonMain = findViewById<Button>(R.id.btnMain)
+
+        buttonMain.setOnClickListener {
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
